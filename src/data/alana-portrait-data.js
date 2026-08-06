97fbd8bb726883e3f40553ebba08161a17faa922
AKVxii <alanakvxii@proton.me>
2026-08-06 10:23:29 -0700

    Wire approved transparent portrait data

--- src/data/alana-portrait-data.js
+++ src/data/alana-portrait-data.js
@@ -1,1 +1,5 @@
-PLACEHOLDER
+import portrait from "./alana-portrait-1.js";
+
+export const ALANA_PORTRAIT_SRC = `data:image/webp;base64,${portrait}`;
+export const ALANA_PORTRAIT_WIDTH = 958;
+export const ALANA_PORTRAIT_HEIGHT = 968;
