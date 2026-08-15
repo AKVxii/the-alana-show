578a2bbf205798ec58d45ed573680641de2277dd
AKVxii <alanakvxii@proton.me>
2026-08-14 18:04:20 -0700

    Simplify merchandise and add nav tab
    * Restore concise merchandise page script
    
    * Add Merchandise to primary navigation
    
    * Add Merchandise to media navigation
    
    * Restore concise five-item merchandise page
    
    * Protect concise five-item public merchandise collection

--- src/components/MediaHeader.js
+++ src/components/MediaHeader.js
@@ -43,6 +43,7 @@ export function MediaHeader() {
         <a href="/topics/">Topics</a>
         <a href="/south-florida/">South Florida</a>
         <a href="/about/">About</a>
+        <a href="/merchandise/">Merchandise</a>
         <a href="/advertise/">Partner</a>
         <a href="/#contact">Contact</a>
         <a class="mobile-listen-live" href="${site.trueOldiesLive}" target="_blank" rel="noopener">Listen Live</a>
