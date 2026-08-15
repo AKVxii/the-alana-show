19b74e12e20206cc1ffa9440f1f81fabf8529cc4
AKVxii <alanakvxii@proton.me>
2026-08-14 18:02:47 -0700

    Add Merchandise to primary navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,6 +26,7 @@ export function Header() {
           <a href="/topics/">Topics</a>
           <a href="/south-florida/">South Florida</a>
           <a href="#about">About</a>
+          <a href="/merchandise/">Merchandise</a>
           <a href="/advertise/">Partner</a>
           <a href="#contact">Contact</a>
           <a class="mobile-listen-live" href="${site.trueOldiesLive}" target="_blank" rel="noopener">Listen Live</a>
