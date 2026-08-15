f48e2a3fa7589605684a267702ab94a2cf77556f
AKVxii <alanakvxii@proton.me>
2026-08-14 18:22:02 -0700

    Load Beyond the Show homepage styles

--- src/beyond-home.js
+++ src/beyond-home.js
@@ -1,5 +1,13 @@
 import { BeyondShow } from "./components/BeyondShow.js";
 
+if (!document.querySelector('link[data-beyond-show-styles]')) {
+  const stylesheet = document.createElement("link");
+  stylesheet.rel = "stylesheet";
+  stylesheet.href = "/src/beyond-show.css?v=1";
+  stylesheet.dataset.beyondShowStyles = "true";
+  document.head.append(stylesheet);
+}
+
 if (!document.querySelector("[data-beyond-home]")) {
   const contact = document.querySelector("#contact");
   if (contact) contact.insertAdjacentHTML("beforebegin", BeyondShow());
