4c29019a330048299190675485ff441f75ab9e03
AKVxii <alanakvxii@proton.me>
2026-08-19 11:18:29 -0700

    Add candidate series to primary navigation

--- src/components/MediaHeader.js
+++ src/components/MediaHeader.js
@@ -41,6 +41,7 @@ export function MediaHeader() {
         <a href="/episodes/">Watch</a>
         <a href="/guests/">Guests</a>
         <a href="/topics/">Topics</a>
+        <a href="/candidates/">Candidates</a>
         <a href="/south-florida/">South Florida</a>
         <a href="/about/">About</a>
         <a href="/merchandise/">Merchandise</a>
