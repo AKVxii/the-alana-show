0b624f99e23baf25c5ecbb06df2e6c1e6f1ec977
AKVxii <alanakvxii@proton.me>
2026-08-26 08:58:39 -0400

    Measure campaign landings on the homepage

--- src/home-entry.js
+++ src/home-entry.js
@@ -1,5 +1,8 @@
+import { setupCampaignMeasurement } from "./lib/campaign-measurement.js";
 import "./featured-video.js";
 import "./main.js";
 import "./merchandise-home.js";
 import "./beyond-home.js";
 import "./home-nav-accessibility.js";
+
+setupCampaignMeasurement();
