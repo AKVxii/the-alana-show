5d5cd9841dbe56f6c51e57b64ddbe962ded3a0c3
AKVxii <alanakvxii@proton.me>
2026-08-26 16:56:45 -0400

    Preserve homepage module contract while venture hub is hidden

--- src/home-entry.js
+++ src/home-entry.js
@@ -2,6 +2,7 @@ import { setupCampaignMeasurement } from "./lib/campaign-measurement.js";
 import "./featured-video.js";
 import "./main.js";
 import "./merchandise-home.js";
+import "./beyond-home.js";
 import "./home-nav-accessibility.js";
 
 setupCampaignMeasurement();
